6f78db0be5676a2619a8d6aacf236b4a42c8bf3b
Naveen Challa <challanaveen03@gmail.com>
2023-10-18 23:50:29 -0700

    Added the ability to ask for primes repeatedly

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the maximum bound of prime numbers");
-        int n = sc.nextInt();
-        Sieve(n);
-
+        int n  = 1;
+        do {
+            System.out.println("Enter the maximum bound of prime numbers. Enter 0 to exit the program.");
+            n = sc.nextInt();
+            Sieve(n);
+        }
+        while (n != 0);
+        System.out.println("Program terminated");
     }
     public static void Sieve(int n) {
         Queue<Integer> numberQueue = new LinkedList<>();
@@ -18,22 +22,25 @@ public class Main {
         for (int i = 2; i <=n; i++) {
             numberQueue.add(i);
         }
-        while (!numberQueue.isEmpty()) {
-            int temp = numberQueue.poll();
-            primeQueue.offer(temp);
+        if (n!=0) {
+            while (!numberQueue.isEmpty()) {
+                int temp = numberQueue.poll();
+                primeQueue.offer(temp);
 
-            int size = numberQueue.size();
-            for (int i = 0; i < size; i++) {
-                int x = numberQueue.poll();
-                if (x % temp != 0) {
-                    numberQueue.offer(x);
+                int size = numberQueue.size();
+                for (int i = 0; i < size; i++) {
+                    int x = numberQueue.poll();
+                    if (x % temp != 0) {
+                        numberQueue.offer(x);
+                    }
                 }
             }
+            System.out.println("Primes up to " + n + ":");
+            while (!primeQueue.isEmpty()) {
+                results.offer(primeQueue.poll());
+            }
+            System.out.println(results);
         }
-        System.out.println("Primes up to " + n + ":");
-        while (!primeQueue.isEmpty()) {
-            results.offer(primeQueue.poll());
-        }
-        System.out.println(results);
+        else return;
     }
 }
